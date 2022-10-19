3e00307ff3dab0ed3f232c8d2b4aaa1e7ed12116
seyfi çapar <giraykut01@gmail.com>
2022-10-19 21:35:55 +0300

    19102022

--- src/test/java/tests/US_16/TC_001.java
+++ src/test/java/tests/US_16/TC_001.java
@@ -1,5 +1,25 @@
 package tests.US_16;
 
-public class TC_001 {
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import page.SpendinGoodPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
+public class TC_001 {
+    SpendinGoodPage spend = new SpendinGoodPage();
+    Actions action = new Actions(Driver.getDriver());
+    ReusableMethods reusable = new ReusableMethods();
+
+    @Test
+    public void maneger() {
+        Driver.getDriver().get("https://spendinggood.com/");
+        spend.SignIn.click();
+        spend.userNameBox.sendKeys("ufukk031@gmail.com");
+        spend.passwordBox.sendKeys("Team05");
+        spend.SignInButton.click();
+        reusable.waitFor(2);
+        spend.myAccount.click();
+    }
 }
